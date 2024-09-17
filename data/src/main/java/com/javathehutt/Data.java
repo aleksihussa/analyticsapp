8a683019782150be1251d6aff2897ce6be692b05
aleksihussa <aleksi.hussa@gmail.com>
2024-09-17 17:23:06 +0300

    add data and service to ui dependencies

--- data/src/main/java/com/javathehutt/Data.java
+++ data/src/main/java/com/javathehutt/Data.java
@@ -1,7 +1,7 @@
 package com.javathehutt;
 
 public class Data {
-  public static void service(String[] args) {
+  public static void service() {
     System.out.println("Data");
   }
 }
--- ui/src/main/java/com/javathehutt/Main.java
+++ ui/src/main/java/com/javathehutt/Main.java
@@ -2,6 +2,8 @@ package com.javathehutt;
 
 public class Main {
   public static void main(String[] args) {
+    Service.service();
+    Data.service();
     System.out.println("This is the whole applciations entrypoint");
   }
 }
