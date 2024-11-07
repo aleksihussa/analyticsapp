87652fd760063d5d288151b9ae9807b4009c44e5
aleksihussa <aleksi.hussa@gmail.com>
2024-11-07 09:44:51 +0200

    rm getters and add first diagram

--- data/src/main/java/com/javathehutt/dto/GDPDto.java
+++ data/src/main/java/com/javathehutt/dto/GDPDto.java
@@ -2,18 +2,13 @@ package com.javathehutt.dto;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
 
 @Data
 @Builder
+@Getter
 public class GDPDto {
+
   private int year;
   private double value;
-
-  public int getYear() {
-    return year;
-  }
-
-  public double getValue() {
-    return value;
-  }
 }
