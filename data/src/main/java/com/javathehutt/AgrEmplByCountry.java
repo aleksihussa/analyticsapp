07c8431507b38ebb790e700e2f936ef858dd984e
aleksihussa <aleksi.hussa@gmail.com>
2024-11-07 09:44:51 +0200

    rm getters and add first diagram

--- data/src/main/java/com/javathehutt/AgrEmplByCountry.java
+++ data/src/main/java/com/javathehutt/AgrEmplByCountry.java
@@ -3,10 +3,12 @@ package com.javathehutt;
 import com.javathehutt.dto.AgrEmplByCountryDto;
 import java.util.ArrayList;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.ToString;
 
 @ToString
 @Builder
+@Getter
 public class AgrEmplByCountry {
   @Builder.Default ArrayList<AgrEmplByCountryDto> values = new ArrayList<AgrEmplByCountryDto>();
 }
--- data/src/main/java/com/javathehutt/GDP.java
+++ data/src/main/java/com/javathehutt/GDP.java
@@ -3,19 +3,16 @@ package com.javathehutt;
 import com.javathehutt.dto.GDPDto;
 import java.util.ArrayList;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.ToString;
 
 @ToString
 @Builder
+@Getter
 public class GDP {
 
   @Builder.Default ArrayList<GDPDto> values = new ArrayList<GDPDto>();
 
   // if ui requires here we can implement methods to manipulate the data
   // or other fields like normalization, etc. can be implemented here
-
-  // Getter for values
-  public ArrayList<GDPDto> getValues() {
-    return values;
-  }
 }
